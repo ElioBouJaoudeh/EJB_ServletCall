de42e8e01f400218e1ea463dcea800d0bf487b05
Elio <elionaoum.boujaoudeh@gmail.com>
2022-03-31 13:59:49 +0300

    Add @EJB annotation before reference

--- src/main/java/com/example/ejb_servletcall/HelloServlet.java
+++ src/main/java/com/example/ejb_servletcall/HelloServlet.java
@@ -3,15 +3,17 @@ package com.example.ejb_servletcall;
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.ejb.EJB;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    @EJB
     private HelloResource bean = new HelloResourceStatelessBean();
 
     public void init() {
         message = bean.hello();
-    } //make "Hello World!" call from the Stateless Session Bean
+    } // make "Hello World!" call from the Stateless Session Bean
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
